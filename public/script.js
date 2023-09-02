e046d3c49bf95ebb34e4ffcd8eab8fb8a7fb7930
Sumaya <136740759+SumayaSaidSelma@users.noreply.github.com>
2023-09-02 11:38:27 -0400

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,22 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('server_info.php')
+        .then(response => response.json())
+        .then(data => displayServerInfo(data))
+        .catch(error => console.error('Error fetching server info:', error));
+});
+
+function displayServerInfo(serverInfo) {
+    const main = document.querySelector('main');
+    main.innerHTML = `
+        <h2>Server Information</h2>
+        <ul>
+            <li><strong>Hostname:</strong> ${serverInfo.hostname}</li>
+            <li><strong>Operating System:</strong> ${serverInfo.os}</li>
+            <li><strong>Kernel Version:</strong> ${serverInfo.kernel_version}</li>
+            <li><strong>CPU Info:</strong> ${serverInfo.cpu_info}</li>
+            <li><strong>Total RAM:</strong> ${serverInfo.ram_total}</li>
+            <li><strong>Disk Usage:</strong> ${serverInfo.disk_usage}</li>
+        </ul>
+    `;
+}
